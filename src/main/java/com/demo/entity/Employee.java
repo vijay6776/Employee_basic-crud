e99eef23f0290ce401688b8b6d6bdb6a1b636ded
vijay6776 <vijay6776.vtalent@gmail.com>
2024-05-01 18:54:49 +0530

    update in pom.xml

--- src/main/java/com/demo/entity/Employee.java
+++ src/main/java/com/demo/entity/Employee.java
@@ -6,21 +6,56 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name="Employee_Info")
 public class Employee {
+	
+	public Employee() {
+		
+	}
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="Emp_Id")
 	private Integer id;
+	
+	public Employee(Integer id, String name, Integer year, String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.year = year;
+		this.gender = gender;
+	}
 	@Column(name="Emp_Name")
     private String name;
 	@Column(name="Joining_Year")
     private Integer year;
 	@Column(name="Emp_Gender")
     private String gender;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
     
 }
